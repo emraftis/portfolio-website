c1513a4be4d1f93e0f7d0fb62ac0aefde10c0ea0
emraftis <82290450+emraftis@users.noreply.github.com>
2021-08-23 16:46:14 -0600

    initial commit
    portfolio website version1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,403 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="svg-icon-list">
+  <div class="hover-icon">
+    <svg height="50" width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0.27 24 24">
+      <title>GitHub</title>
+      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+    </svg>
+  </div>
+  <div class="hover-icon">
+    <svg height="50" width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24">
+      <title>LinkedIn</title>
+      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+    </svg>
+  </div>
+  <div class="hover-icon">
+    <svg height="50" width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24">
+      <title>Instagram</title>
+      <path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/>
+    </svg>
+  </div>
+  <div class="hover-icon">
+    
+    <svg height="50" width="50" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 299.998 299.998" style="enable-background:new 0 0 299.998 299.998;" xml:space="preserve">
+      <title>Resume</title>
+      <g>
+        <g>
+          <path d="M149.995,0.001C67.156,0.001,0,67.16,0,149.999s67.156,149.997,149.995,149.997s150.003-67.161,150.003-149.997    C299.997,67.157,232.834,0.001,149.995,0.001z M217.205,217.204c0,10.066-8.159,18.225-18.223,18.225h-97.967    c-10.068,0-18.225-8.159-18.225-18.225V82.79c0-10.066,8.159-18.225,18.225-18.225h76.892v36.45h39.298V217.204z M185.688,93.232    V64.563l31.517,28.669H185.688z"/>
+        </g>
+      </g>
+    </svg>
+  </div>
+</div>
+
+<div class="workspace">
+
+
+  <div class="landing-page">
+    <div class="intro-content">
+      <div class="intro-text">
+        <h2 class="main-intro-text">Hi, I'm Evan Raftis.</h2>
+        <p class="base-intro-text">- a <span class="special-base-text">Finance Professional</span> looking for opportunities in <span class="special-base-text">Fintech</span> and <span class="special-base-text">Web Development!</span></p>
+      </div>
+      <div class="down-arrow">
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+          <g>
+            <g>
+              <path d="M511.224,129.196c-1.579-3.81-5.297-6.296-9.423-6.296H10.199c-4.126,0-7.844,2.486-9.423,6.296    c-1.578,3.811-0.705,8.198,2.211,11.115l245.801,245.801c1.992,1.992,4.601,2.987,7.212,2.987c2.611,0,5.22-0.995,7.212-2.987    l245.801-245.801C511.928,137.394,512.802,133.008,511.224,129.196z M256,364.477L34.823,143.299h442.355L256,364.477z"/>
+            </g>
+          </g>
+          <g>
+            <g>
+              <path d="M225.766,285.453l-84.944-84.944c-3.985-3.983-10.441-3.983-14.425,0c-3.983,3.984-3.983,10.442,0,14.425l84.944,84.944    c1.992,1.992,4.601,2.987,7.212,2.987c2.611,0,5.22-0.995,7.213-2.987C229.749,295.894,229.749,289.436,225.766,285.453z"/>
+            </g>
+          </g>
+          <g>
+            <g>
+              <path d="M259.475,319.161l-4.719-4.719c-3.985-3.983-10.441-3.983-14.425,0c-3.983,3.984-3.983,10.442,0,14.425l4.719,4.719    c1.992,1.992,4.601,2.987,7.212,2.987c2.611,0,5.22-0.995,7.213-2.987C263.458,329.602,263.458,323.144,259.475,319.161z"/>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="page-container" id="about-page">
+    <h2 class="container-header">About Me</h2>
+    
+    <div class="page-content">
+      <div class="profile-photo-container">
+        <img src="./assets/evanProfile.jpg" alt="Photo of Evan">
+      </div>
+      <ul>
+        <li>I'm a Canadian full-stack web developer - solving problems with NodeJS, Express, MongoDB, and VueJS. I consider myself a lifelong learner 
+        and I'm looking for a new challenge after 10 years in Commercial Finance. I have a passion for travel and I love to create things</li>
+      </ul>
+    </div>
+  </div>
+
+
+  <div class="page-container" id="projects-page">
+    <h2 class="container-header">My Projects</h2>
+    <div class="project-container">
+      <ul>
+        <li><div class="project-card">First</div></li>
+        <li><div class="project-card">Second</div></li>
+        <li><div class="project-card">Third</div></li>
+      </ul>
+    </div>
+  </div>
+
+
+  <div class="page-container" id="contact-page">
+    <h2 class="container-header">Contact Me</h2>
+    <button class="contact-btn"><span class="strong-text">Let's Chat</span></button>
+  </div>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  font-size: 100%;
+  vertical-align: baseline;
+  background: transparent;
+  font-family: 'Ubuntu', sans-serif;
+  font-weight: 300;
+}
+
+.intro-text {
+  color: rgb(205, 196, 245);
+}
+
+.down-arrow {
+  fill: rgb(205, 196, 245);
+  position: absolute;
+  bottom: 15vh;
+  left: 50%;
+  margin-left: -50px;
+}
+
+.landing-page {
+  background-color: rgba(15, 0, 39, 0.911);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 85vh;
+}
+
+.landing-page p {
+  font-size: 1.5rem;
+  width: 60%;
+}
+
+.intro-content {
+  display: flex;
+  justify-content: center;
+}
+
+.main-intro-text {
+  font-family: 'Rampart One', cursive;
+  color: white;
+  font-size: 5rem;
+}
+
+.special-base-text {
+  font-weight: 400;
+  font-style: italic;
+  color: rgb(137, 123, 201);
+  font-size: 110%;
+  padding-left: 0.25rem;
+  padding-right: 0.25rem;
+}
+
+.svg-icon-list {
+  position: fixed;
+  bottom: 0rem;
+  margin-left: 5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  background-color: white;
+  height: 75vh;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.hover-icon {
+  margin-top: 2rem;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.hover-icon svg {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.hover-icon:hover {
+  box-shadow: 0 -0.5rem 2rem rgba(4, 4, 71, 0.85);
+  border-radius: 5rem;
+}
+
+ul {
+  list-style: none;
+}
+
+.page-container {
+  height: 100%;
+  font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 7rem;
+  padding-left: 30rem;
+  padding-right: 30rem;
+}
+
+.container-header {
+  font-size: 200%;
+  padding-bottom: 2rem;
+}
+
+.project-container ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.project-card {
+  border: 1px solid black;
+  box-shadow: 0.35rem 0.25rem 0.5rem rgba(15, 0, 39, 0.5)
+}
+
+#about-page {
+  background-color: white;
+  background-repeat: no-repeat;
+  background-size: auto 100%;
+  background-position-x: center;
+}
+
+.page-content {
+  display: flex;
+  font-size: 1.5rem;
+}
+.page-content ul {
+  margin-left: 2rem;
+}
+
+img {
+  max-width: 15rem;
+  height: auto;
+  padding-right: 3rem;
+  border-radius: 50%;
+}
+
+#projects-page {
+  background-color: white;
+  background-repeat: no-repeat;
+  background-size: auto 100%;
+  background-position-x: center;
+}
+
+#contact-page {
+  background-color: white;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-x: center;
+}
+
+.contact-btn {
+  background-color: rgba(109, 124, 253, 0.8);
+  display: inline-block;
+  cursor: pointer;
+  max-width: 10rem;
+  padding: 1rem;
+  border-radius: 15px;
+  font-size: 75%;
+}
+
+@media screen and (max-width: 699px) {
+  .landing-page {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    min-height: 85vh;
+  }
+  .main-intro-text{
+    font-size: 2.5rem;
+  }
+  .landing-page p {
+    font-size: 100%;
+    width: 100%;
+  }
+  .svg-icon-list {
+    display: none;
+  }
+  .down-arrow {
+    bottom: 5vh;
+  }
+  .page-container {
+    height: 100%;
+    font-size: 2rem;
+    padding: 5rem;
+    padding-bottom: 0;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  .page-container ul {
+    margin-top: 2rem;
+    padding-right: 2rem;
+  }
+  .page-content {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+
+@media screen and (min-width: 700px) {
+  .landing-page {
+    padding-left: 5rem;
+    padding-right: 5rem;
+    min-height: 85vh;
+  }
+  .main-intro-text{
+    font-size: 3rem;
+  }
+  .landing-page p {
+    font-size: 120%;
+    width: 85%;
+  }
+  .svg-icon-list {
+    display: none;
+  }
+  .page-container {
+    height: 100%;
+    font-size: 2rem;
+    padding: 7rem;
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
+}
+
+
+
+
+@media screen and (min-width: 1210px) {
+  .landing-page {
+    padding-left: 15rem;
+    padding-right: 10rem;
+    min-height: 85vh;
+  }
+  .main-intro-text{
+    font-size: 5rem;
+  }
+  .landing-page p {
+    font-size: 150%;
+    width: 75%;
+  }
+  .svg-icon-list {
+    display: inline-block;
+    transform: scale(1);
+    margin-left: 3rem;
+  }
+  .project-container ul {
+    width: 75%;
+  }
+  .page-container {
+    height: 100%;
+    font-size: 2rem;
+    padding: 7rem;
+    padding-left: 15rem;
+    padding-right: 10rem;
+  }
+}
+
+
+
+
+@media screen and (min-width: 1650px) {
+  .landing-page {
+    padding-left: 20rem;
+    padding-right: 15rem;
+    min-height: 85vh;
+  }
+  .main-intro-text{
+    font-size: 6rem;
+  }
+  .landing-page p {
+    font-size: 175%;
+    width: 60%;
+  }
+  .svg-icon-list {
+    display: inline-block;
+    transform: scale(1);
+    margin-left: 5rem;
+  }
+  .page-container {
+    font-size: 2rem;
+    padding: 7rem;
+    padding-left: 30rem;
+    padding-right: 30rem;
+  }
 }
 </style>
